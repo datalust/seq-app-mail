2012b656eb1239ffd861509f3667b52c192db204
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2023-08-23 09:33:34 +1000

    Switch to Microsoft Graph API for Microsoft 365 mail

--- src/Seq.App.Mail.Microsoft365/Microsoft365MailApp.cs
+++ src/Seq.App.Mail.Microsoft365/Microsoft365MailApp.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
 using MimeKit;
 using Seq.Apps;
 using Seq.Mail;
+
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 
 namespace Seq.App.Mail.Microsoft365
 {
@@ -12,15 +18,80 @@ namespace Seq.App.Mail.Microsoft365
     public class Microsoft365MailApp: MailApp
     {
         Microsoft365Options? _options;
+        
+        [SeqAppSetting(
+            DisplayName = "Tenant id",
+            HelpText = "The directory (tenant) id that will be used to send mail.")]
+        public string? TenantId { get; set; }
+        
+        [SeqAppSetting(
+            DisplayName = "Client id",
+            HelpText = "The application (client) id of an app registration with the `Mail.Send` permission for the Microsoft Graph API.")]
+        public string? ClientId { get; set; }
 
+        [SeqAppSetting(
+            DisplayName = "Client secret",
+            InputType = SettingInputType.Password,
+            HelpText = "A client secret for the app registration.")]
+        public string? ClientSecret { get; set; }
+        
+        [SeqAppSetting(
+            IsOptional = true,
+            DisplayName = "Save to sent items",
+            HelpText = "If set, outgoing email will be saved to the `From` user's sent items.")]
+        public bool SaveToSentItems { get; set; }
+        
         protected override void OnAttached()
         {
             base.OnAttached();
+
+            _options = new Microsoft365Options(
+                TenantId ?? throw new InvalidOperationException("A tenant id is required."),
+                ClientId ?? throw new InvalidOperationException("A client id is required."),
+                ClientSecret ?? throw new InvalidOperationException("A client secret is required."),
+                SaveToSentItems);
         }
 
         protected override async Task SendAsync(MimeMessage message, CancellationToken cancel)
         {
-            throw new NotImplementedException();
+            // This implementation follows the article at:
+            //   https://medium.com/medialesson/how-to-send-emails-in-net-with-the-microsoft-graph-a97b57430bbd
+            
+            var credential = new ClientSecretCredential(_options!.TenantId, _options.ClientId, _options.ClientSecret);
+            using var graphClient = new GraphServiceClient(credential);
+
+            var graphMessage = new Message
+            {
+                Subject = message.Subject,
+                Body = new ItemBody
+                {
+                    ContentType = message.HtmlBody != null ? BodyType.Html : BodyType.Text,
+                    Content = message.HtmlBody ?? message.TextBody
+                },
+                From = new Recipient
+                {
+                    EmailAddress = AsGraphEmailAddress(message.From.Single()) 
+                },
+                ToRecipients = message.To.Cast<MailboxAddress>().Select(to =>
+                    new Recipient
+                    {
+                        EmailAddress = AsGraphEmailAddress(to)
+                    }).ToList()
+            };
+
+            await graphClient.Users[From]
+                .SendMail
+                .PostAsync(new() { Message = graphMessage, SaveToSentItems = _options.SaveToSentItems }, cancellationToken: cancel);
+        }
+
+        static EmailAddress AsGraphEmailAddress(InternetAddress address)
+        {
+            var mailboxAddress = (MailboxAddress)address;
+            return new EmailAddress
+            {
+                Name = mailboxAddress.Name,
+                Address = mailboxAddress.Address
+            };
         }
     }
 }
--- src/Seq.App.Mail.Microsoft365/Microsoft365Options.cs
+++ src/Seq.App.Mail.Microsoft365/Microsoft365Options.cs
@@ -2,5 +2,16 @@
 
 class Microsoft365Options
 {
-    
+    public string TenantId { get; }
+    public string ClientId { get; }
+    public string ClientSecret { get; }
+    public bool SaveToSentItems { get; }
+
+    public Microsoft365Options(string tenantId, string clientId, string clientSecret, bool saveToSentItems)
+    {
+        TenantId = tenantId;
+        ClientId = clientId;
+        ClientSecret = clientSecret;
+        SaveToSentItems = saveToSentItems;
+    }
 }
