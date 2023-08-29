450deb4d3b7fd1cbfdb8fdecf0a114f863e1d0b1
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2023-08-29 19:48:38 +1000

    Skip certificate checking when protocol security is None'

--- src/Seq.App.Mail.Smtp/SmtpOptions.cs
+++ src/Seq.App.Mail.Smtp/SmtpOptions.cs
@@ -9,16 +9,18 @@ class SmtpOptions
     public int Port { get; }
     public string? Username { get; }
     public string? Password { get; }
+    public bool DisableCertificateValidation { get; }
     public SecureSocketOptions SocketOptions { get; }
 
     public bool RequiresAuthentication => !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password);
 
-    public SmtpOptions(string host, int port, SecureSocketOptions socketOptions, string? username, string? password)
+    public SmtpOptions(string host, int port, SecureSocketOptions socketOptions, string? username, string? password, bool disableCertificateValidation)
     {
         Host = host ?? throw new ArgumentNullException(nameof(host));
         Port = port;
         Username = username;
         Password = password;
+        DisableCertificateValidation = disableCertificateValidation;
         SocketOptions = socketOptions;
     }
 }
